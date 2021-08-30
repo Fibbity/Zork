7a8dc588169796b2a1ccce76cd5ee391af4b057a
Wi024933 <beattywf@knights.ucf.edu>
2021-08-30 14:52:52 -0400

    1.3 Satisfied
    Removed Code Rot
    Updated Code

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -28,50 +28,30 @@ namespace Zork
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString = null; //Do I really need to make this null?
+                string outputString;
                 switch (command)
                 {
                     case Commands.LOOK:
                         command = Commands.LOOK;
-                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door.");
-
-                        //outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door."; //Is this what's desired?
+                        outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door.";
 
                         break;
 
                     case Commands.NORTH:
-                        command = Commands.NORTH;
-                        Console.WriteLine("You moved NORTH");
-
-                        break;
-
                     case Commands.EAST:
-                        command = Commands.EAST;
-                        Console.WriteLine("You moved EAST");
-
-                        break;
-
                     case Commands.SOUTH:
-                        command = Commands.SOUTH;
-                        Console.WriteLine("You moved SOUTH");
-
-                        break;
-
                     case Commands.WEST:
-                        command = Commands.WEST;
-                        Console.WriteLine("You moved WEST");
+                        outputString = "You moved " + command;
 
                         break;
 
                     case Commands.QUIT:
-                        command = Commands.QUIT;
-                        Console.WriteLine("Thank you for playing!");
+                        outputString = "Thank you for playing!";
 
                         break;
 
                     default:
-                        command = Commands.UNKNOWN;
-                        Console.WriteLine("Unrecognized Command");
+                        outputString = "Unrecognized Command";
 
                         break;
 
@@ -80,28 +60,6 @@ namespace Zork
                 Console.WriteLine(outputString);
 
             }
-
-            /*
-            string inputString = Console.ReadLine(); //Also establishes inputString variable
-
-
-            inputString = inputString.ToUpper();
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-
-            }
-            else if (inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field West of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork' lies by the door.");
-
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-
-            }
-            */
 
         }//END Main
 
