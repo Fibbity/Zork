0901bba5d677288566906bead3ed9b2cd7b3762b
Wi024933 <beattywf@knights.ucf.edu>
2021-08-30 14:16:11 -0400

    1.3 Satisfied maybe?
    Need to double check with Professor on outputString
    - OutputString throws error being an unassigned local variable unless set to null
    - Commented out code might be what is desired

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -22,6 +22,66 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString = null; //Do I really need to make this null?
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        command = Commands.LOOK;
+                        Console.WriteLine("A rubber mat saying 'Welcome to Zork!' lies by the door.");
+
+                        //outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door."; //Is this what's desired?
+
+                        break;
+
+                    case Commands.NORTH:
+                        command = Commands.NORTH;
+                        Console.WriteLine("You moved NORTH");
+
+                        break;
+
+                    case Commands.EAST:
+                        command = Commands.EAST;
+                        Console.WriteLine("You moved EAST");
+
+                        break;
+
+                    case Commands.SOUTH:
+                        command = Commands.SOUTH;
+                        Console.WriteLine("You moved SOUTH");
+
+                        break;
+
+                    case Commands.WEST:
+                        command = Commands.WEST;
+                        Console.WriteLine("You moved WEST");
+
+                        break;
+
+                    case Commands.QUIT:
+                        command = Commands.QUIT;
+                        Console.WriteLine("Thank you for playing!");
+
+                        break;
+
+                    default:
+                        command = Commands.UNKNOWN;
+                        Console.WriteLine("Unrecognized Command");
+
+                        break;
+
+                }
+
+                Console.WriteLine(outputString);
+
+            }
+
+            /*
             string inputString = Console.ReadLine(); //Also establishes inputString variable
 
 
@@ -41,7 +101,7 @@ namespace Zork
                 Console.WriteLine("Unrecognized command.");
 
             }
-
+            */
 
         }//END Main
 
