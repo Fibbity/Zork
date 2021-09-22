806238f56fb9f85babd3bbdace6bee168b7d0548
Wi024933 <beattywf@knights.ucf.edu>
2021-09-22 13:54:31 -0400

    Moved Assert to its own file

---
+++ Zork/Assert.cs
@@ -0,0 +1,21 @@
+﻿using Zork;
+using System.Diagnostics;
+using System;
+
+//--------------------------//
+public static class Assert
+//--------------------------//
+{
+    [Conditional("DEBUG")]
+
+    //--------------------------//
+    public static void IsTrue(bool expression, string message = null)
+    //--------------------------//
+    {
+        if (expression == false)
+        {
+            throw new Exception(message);
+        }
+
+    }//END IsTrue
+}
