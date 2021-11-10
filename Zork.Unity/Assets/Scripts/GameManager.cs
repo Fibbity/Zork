54c57495f543b798600fc8a326b6b311712ed839
Wi024933 <beattywf@knights.ucf.edu>
2021-11-09 22:28:46 -0500

    Pre Bringing Zork and Unity really together

---
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+
+
+    //-----------------------//
+    void Awake()
+    //-----------------------//
+    {
+
+    }//END Awake
+
+    //-----------------------//
+    void Start()
+    //-----------------------//
+    {
+
+    }//END Start
+
+    //-----------------------//
+    void Update()
+    //-----------------------//
+    {
+
+    }//END Update
+
+}//END GameManager
